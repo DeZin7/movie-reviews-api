d61ba5d54759a3834656bbea8578a6a1cd1712ca
DeZin7 <marcusandre77@icloud.com>
2023-04-05 09:48:01 -0700

    Adding signup view.

--- app/movie/views.py
+++ app/movie/views.py
@@ -1,10 +1,25 @@
+"""
+Views for the movie APIs.
+"""
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Marcus Andre'})
+    """Retrieve the values submited."""
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm': searchTerm})
 
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
+
+
+def signup(request):
+    """
+    Retrieve the email from the GET request (request.GET.get('email'))
+    and send it to signup.html by passing in a key value dictionary,
+    {'email': email}
+    """
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
