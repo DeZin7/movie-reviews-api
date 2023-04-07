c2b5f6e1e18d863d054f0d9dd3847d43b72af8cc
DeZin7 <marcusandre77@icloud.com>
2023-04-07 12:17:10 -0700

    Adding News app.

--- app/core/models.py
+++ app/core/models.py
@@ -6,3 +6,9 @@ class Movie(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
+
+
+class News(models.Model):
+    headline = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateField()
