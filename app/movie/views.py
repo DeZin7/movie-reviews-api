fd217401893e005ac2bf9be1c4f95080840c08dc
DeZin7 <marcusandre77@icloud.com>
2023-04-07 11:44:54 -0700

    Implementing a search.

--- app/movie/views.py
+++ app/movie/views.py
@@ -9,7 +9,10 @@ from core.models import Movie
 def home(request):
     """Retrieve the values submited."""
     searchTerm = request.GET.get('searchMovie')
-    movies = Movie.objects.all()
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
     return render(request, 'home.html',
                   {'searchTerm': searchTerm, 'movies': movies})
 
