28bbc0e65c3f31482ba0156f1c594118754479a3
DeZin7 <marcusandre77@icloud.com>
2023-04-08 12:43:59 -0700

    Serving static files.

--- app/app/settings.py
+++ app/app/settings.py
@@ -132,3 +132,7 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'app/static/',
+]
