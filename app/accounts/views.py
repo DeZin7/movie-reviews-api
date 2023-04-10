2c76ad55752430ae5e4cd0886771da5288d4f6e2
DeZin7 <marcusandre77@icloud.com>
2023-04-09 20:24:30 -0700

    Handling user creation errors.

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.shortcuts import redirect
+from django.db import IntegrityError
 
 
 def signupaccount(request):
@@ -11,10 +12,19 @@ def signupaccount(request):
                       {'form': UserCreationForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                request.POST['username'],
-                password=request.POST['password1'])
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'],
+                    password=request.POST['password1'])
 
-            user.save()
-            login(request, user)
-            return redirect('home')
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signupaccount.html',
+                              {'form': UserCreationForm,
+                               'error': 'Username already exists. Choose new username.'}) # noqa
+        else:
+            return render(request, 'signupaccount.html',
+                          {'form': UserCreationForm,
+                           'error': 'Passwords do not match'})
