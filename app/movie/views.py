d4ea8244a7fa615b3df9126b42c2e9646b39ee3a
DeZin7 <marcusandre77@icloud.com>
2023-04-11 10:49:49 -0700

    Creating Review.

--- app/movie/views.py
+++ app/movie/views.py
@@ -3,8 +3,10 @@ Views for the movie APIs.
 """
 from django.shortcuts import render
 from django.http import HttpResponse
-from core.models import Movie
-from django.shortcuts import get_object_or_404
+from core.models import Movie, Review
+from django.shortcuts import get_object_or_404, redirect
+from .forms import ReviewForm
+
 
 
 def home(request):
@@ -33,6 +35,28 @@ def signup(request):
 
 
 def detail(request, movie_id):
+    """Show movie details."""
     movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'detail.html',
                   {'movie': movie})
+
+
+def createreview(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'GET':
+        return render(request, 'createreview.html',
+                      {'form': ReviewForm(), 'movie': movie})
+    else:
+        try:
+            form = ReviewForm(request.POST)
+            newReview = form.save(commit=False)
+            newReview.user = request.user
+            newReview.movie = movie
+            newReview.save()
+            return redirect('detail',
+                            newReview.movie.id)
+        except ValueError:
+            return render(request,
+                          'createreview.html',
+                          {'form': ReviewForm(), 'error':
+                           'bad data passed in.'})
